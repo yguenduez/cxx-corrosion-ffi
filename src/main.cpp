e099600aa5628792ebbc678aace42a7d49a9ad5b
ysngndz <yasinguenduez1@gmail.com>
2023-02-25 17:11:07 +0100

    cpp_part: use the shared structure

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,18 @@
 #include <string>
 
 int main() {
+  // Opaque type
   auto fizzbuzzer = new_fizz_buzz();
   const auto result = fizzbuzzer->to_fizz_buzz(45);
-
   std::cout << std::string{result} << std::endl;
+
+  // Shared struct defined in ffi module
+  DataBlob blob{};
+  blob.mini_blobs.push_back(0x01);
+  blob.mini_blobs.push_back(0x02);
+  for (auto const& miniBlob: blob.mini_blobs){
+    std::cout << std::to_string(miniBlob) << std::endl;
+  }
 
   return 0;
 }
