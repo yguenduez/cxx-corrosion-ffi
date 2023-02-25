fbebcc763d41122fe11368702e2df71729e8a039
ysngndz <yasinguenduez1@gmail.com>
2023-02-25 17:08:07 +0100

    rust_part: clean up the ffi part

--- src/rusty_code/src/lib.rs
+++ src/rusty_code/src/lib.rs
@@ -4,13 +4,32 @@ use fizz_buzz::FizzBuzz;
 
 #[cxx::bridge]
 mod ffi {
+    // Shared structs: fields are visible to both languages
+    // One source of truth
+    pub struct DataBlob {
+        pub total_size: usize,
+        mini_blobs: Vec<u8>,
+    }
+
+    // Opaque types: fields are secret to other language.
+    // Cannot be passed as value!
     extern "Rust" {
         type FizzBuzz;
         fn to_fizz_buzz(self: &FizzBuzz, input: u32) -> String;
+    }
+
+    // Functions: implemented in either language - callable from the other
+    extern "Rust" {
         fn new_fizz_buzz() -> Box<FizzBuzz>;
     }
 }
 
-fn new_fizz_buzz() -> Box<FizzBuzz>{
+// use the shared DataBlob struct from the ffi module
+use ffi::DataBlob;
+fn function_taking_a_blob(datablob: &DataBlob) -> usize {
+    datablob.total_size
+}
+
+fn new_fizz_buzz() -> Box<FizzBuzz> {
     Box::new(FizzBuzz)
 }
